7100d90161e4c29c975158c342b62ce9f0ed521c
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-10-09 21:03:49 +0300

    generate new components

---
+++ src/components/ContactListBody.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ContactListBody extends React.Component{
+
+}
+
+export default ContactListBody
---
+++ src/components/ContactListHeader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ContactListHeader extends React.Component{
+
+}
+
+export default ContactListHeader
