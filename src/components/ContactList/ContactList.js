910a26e37e9f0cb0fa7f73b44dedfaeef14f6181
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-10-15 21:30:35 +0300

    init hometask 2

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ContactItem from './ContactItem'
+import PropTypes from 'prop-types'
+
+class ContactList extends React.Component {
+  render () {
+    const { contacts } = this.props
+    return (
+      <ul>
+        {contacts.map(contact => (
+          <ContactItem contact={contact} />
+        ))}
+      </ul>
+    )
+  }
+}
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.shape({
+    surname: PropTypes.string,
+    name: PropTypes.string
+  })).isRequired,
+}
+
+ContactList.defaultProps = {
+  contacts: [],
+}
+
+export default ContactList
