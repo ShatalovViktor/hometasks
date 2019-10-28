aded5a9659ef7ea7f3bb4c6066eac730f099a70c
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-10-28 19:51:56 +0200

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Header.css'
+
+Header.propTypes = {
+
+}
+
+function Header (props) {
+  return (
+    <header className="App-header">
+      <button >Add</button>
+    </header>
+  )
+}
+
+export default Header
