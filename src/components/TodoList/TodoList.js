ac26c4ea799aab41308a8f299fb866a168b9cc23
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-10-28 20:09:28 +0200

    add todolist component and popup extension

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+TodoList.propTypes = {
+  
+}
+
+function TodoList (props) {
+  return (
+    <div></div>
+  )
+}
+
+export default TodoList
