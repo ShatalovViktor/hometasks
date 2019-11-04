3a32aaaec85fc9b41591297113bafbe216db224a
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-04 21:03:35 +0200

    add style for menu

--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
-Header.propTypes = {
-  
-}
+Header.propTypes = {}
 
 function Header (props) {
   return (
-    <header>
-      <ul>
-        <li><NavLink exact to="/" activeClassName='active'>Home</NavLink></li>
-        <li><NavLink to="/users" activeClassName='active'>Users</NavLink></li>
-        <li><NavLink to="/albums" activeClassName='active'>Albums</NavLink></li>
-      </ul>
+    <header className='topnav'>
+      <NavLink exact to="/" activeClassName='active'>Home</NavLink>
+      <NavLink to="/users" activeClassName='active'>Users</NavLink>
+      <NavLink to="/albums" activeClassName='active'>Albums</NavLink>
     </header>
   )
 }
