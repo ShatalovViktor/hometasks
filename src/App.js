c90ad6eb44e3c2e06f8d280c1b9efbb09aeb573c
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-04 20:14:44 +0200

    init hometask 5

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
-import Header from './components/Header/Header'
-import TodoList from './components/TodoList/TodoList'
-import { ThemeContext, themes } from './contexts/theme'
+import Home from './Home'
+import About from './About'
+import Header from './Header'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import Users from './Users/'
 
 function App () {
-  const [todos, setTodos] = useState([])
-  const [theme, setTheme] = useState(themes.light)
-
-  function addTodoItem (item) {
-    setTodos([...todos, item])
-  }
-
-  function deleteTodoItem (item) {
-    const newTodos = todos.filter(el => el !== item)
-    setTodos(newTodos)
-  }
-
-  function onDoneClick (item) {
-    const itemTodo = { ...item, isDone: !item.isDone }
-    const newTodos = todos.map(el => el.id === itemTodo.id ? itemTodo : el)
-    setTodos(newTodos)
-  }
-
-  function handleChangeTheme () {
-    setTheme(theme === themes.light ? themes.dark : themes.light)
-  }
-
   return (
-    <div className="App">
-      <ThemeContext.Provider value={theme}>
-        <Header addTodoItem={addTodoItem} onThemeChange={handleChangeTheme}/>
-        <TodoList todos={todos} onDeleteTodo={deleteTodoItem} onDoneClick={onDoneClick} />
-      </ThemeContext.Provider>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Switch>
+          <Route path="/about" component={About}/>
+          <Route path="/users" component={Users}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
