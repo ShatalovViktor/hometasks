81034f6ec24bde45fa939a0cf64294c3393bfb11
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-09 17:40:53 +0200

    create posts components

---
+++ src/components/PostDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+PostDetail.propTypes = {
+  
+}
+
+function PostDetail (props) {
+  return (
+    <div></div>
+  )
+}
+
+export default PostDetail
---
+++ src/components/PostList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+PostList.propTypes = {
+
+}
+
+function PostList (props) {
+  return (
+    <div></div>
+  )
+}
+
+export default PostList
