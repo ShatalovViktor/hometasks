73ffaed4aa8d5a68f0995027abe1a4f736bb0b60
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-09 18:39:41 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
+import { Toolbar, Typography, AppBar, Grid } from '@material-ui/core'
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles'
 
-function App() {
+import { deepOrange } from '@material-ui/core/colors'
+import './App.css'
+import PostList from './components/PostList'
+import PostDetail from './components/PostDetail'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
+
+function App () {
+  const theme = createMuiTheme({
+    palette: {
+      primary: deepOrange,
+    },
+  })
+
+  const classes = useStyles(theme)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6">
+                  Posts
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Switch>
+              <Route path='/posts/:id' component={PostDetail} />
+              <Route path='/posts' component={PostList} />
+              <Route path='/' render={() => <Redirect to="/posts" />} />
+            </Switch>
+          </Grid>
+        </Grid>
+      </Router>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
