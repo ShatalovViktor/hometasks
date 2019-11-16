d3f7d0d9f7ba5f88eb4f4a641d73db7e1dab8854
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-16 21:06:50 +0200

    add grid

--- src/components/Todo/List.js
+++ src/components/Todo/List.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
+import ThemeContext from '../../theme'
+import Item from './Item'
 
-List.propTypes = {
+List.propTypes = {}
 
-}
-
-function List ({todos, remove, toggle}) {
+function List ({ todos, remove, toggle }) {
+  const theme = useContext(ThemeContext)
   return (
-    <div>
-      {todos.map(todo => (
-        <div
-          style={{
-            textDecoration: todo.isDone ? 'line-through' : 'none'
-          }}
-          key={todo.id}
-          onClick={toggle.bind(null, todo)}
-        >
-          {todo.title}
-        </div>
-      ))}
-    </div>
+    <Grid container className={theme.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          {todos.map(todo => (
+            <Item key={todo.d} todo={todo} onDone={toggle} onDelete={remove}/>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
