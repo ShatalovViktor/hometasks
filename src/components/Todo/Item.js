9eb9c7faf0f1fe2b9b3c83e81c1599a22882f8fd
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-16 21:06:33 +0200

    add delete

---
+++ src/components/Todo/Item.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Paper, Typography, Checkbox, FormControlLabel, Chip } from '@material-ui/core'
+import ThemeContext from '../../theme'
+
+Item.propTypes = {}
+
+function Item ({ todo, onDone, onDelete }) {
+  const theme = useContext(ThemeContext)
+  return (
+    <Grid item xs={4}>
+      <Paper key={todo.id} className={theme.paper}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={todo.isDone}
+              onChange={onDone.bind(null, todo)}
+              value={todo.id} inputProps={{
+              'aria-label': 'primary checkbox',
+            }}
+            />
+          }
+          label={todo.title}
+        />
+        <Chip onDelete={onDelete.bind(null, todo.id)} color='secondary' />
+      </Paper>
+    </Grid>
+  )
+}
+
+export default Item
