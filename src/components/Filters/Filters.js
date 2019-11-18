4c921e2890b891a01339ccf09138992bcdad95fb
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-18 20:41:07 +0200

    fix layout

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,23 +1,39 @@
 import React from 'react'
-import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
+import { FormControl, InputLabel, Select, MenuItem, makeStyles, Grid } from '@material-ui/core'
 import { FILTER_ALL, FILTER_NOT_DONE, FILTER_DONE } from '../../filters'
 import PropTypes from 'prop-types'
 
 Filters.propTypes = {}
+const useStyles = makeStyles(theme => ({
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '100%',
+      '&:focus': {
+        width: '100%',
+      },
+    },
+  },
+}))
 
 function Filters (props) {
+  const classes = useStyles()
   return (
-    <FormControl>
-      <InputLabel id="demo-simple-select-label">Filter</InputLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-      >
-        <MenuItem value={FILTER_ALL}>All</MenuItem>
-        <MenuItem value={FILTER_DONE}>Completed</MenuItem>
-        <MenuItem value={FILTER_NOT_DONE}>Not completed</MenuItem>
-      </Select>
-    </FormControl>
+    <Grid item>
+      <FormControl>
+        <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+        <Select className={classes.inputInput}
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+        >
+          <MenuItem value={FILTER_ALL}>All</MenuItem>
+          <MenuItem value={FILTER_DONE}>Completed</MenuItem>
+          <MenuItem value={FILTER_NOT_DONE}>Not completed</MenuItem>
+        </Select>
+      </FormControl>
+    </Grid>
   )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import Filters from '../Filters/Filters'
 
@@ -7,10 +7,18 @@ function Header () {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">
-          Redux todo list
-        </Typography>
-        <Filters />
+        <Grid
+          justify="space-between"
+          container
+          spacing={24}
+        >
+          <Grid item>
+            <Typography variant="h6">
+              Redux todo list
+            </Typography>
+          </Grid>
+          <Filters />
+        </Grid>
       </Toolbar>
     </AppBar>
   )
