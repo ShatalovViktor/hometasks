6dc97d65f3dde95c45e45c251009dae198d99eda
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-11-19 09:16:34 +0200

    begin form

--- src/components/Todo/Form.js
+++ src/components/Todo/Form.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Formik, Field, ErrorMessage } from 'formik'
+import { TextField, Button } from '@material-ui/core'
 
 Form.propTypes = {
-
+  add: PropTypes.func,
 }
 
-function Form ({add}) {
+function Form ({ add }) {
+  function validateRequireField (value) {
+    return !value ? 'Message is required' : false
+  }
+
+  function onFormSubmit(values, { resetForm }){
+    // onSubmit(values);
+    resetForm();
+  }
   return (
-    <div>Form</div>
+    <Formik
+      initialValues={{title:''}}
+      onSubmit={onFormSubmit}
+    >
+      <Form autoComplete="off">
+        <div>
+          <Field
+            name="title"
+            label="Message"
+            multiline
+            fullWidth
+            as={TextField}
+            validate={validateRequireField}
+          />
+          <ErrorMessage name="body" component="div" />
+        </div>
+        <div style={{ paddingTop: '10px' }}>
+          <Button type="submit" variant="contained" color="primary">
+            Save
+          </Button>
+        </div>
+      </Form>
+
+    </Formik>
   )
 }
 
